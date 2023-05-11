f1db956f8128cfd72591e84b303b77df55cecb35
Dvir Hazut <53955899+Dvir1992@users.noreply.github.com>
2023-05-11 15:53:21 +0300

    Create env.sv

---
+++ env.sv
@@ -0,0 +1,28 @@
+class env extends uvm_env;
+`uvm_component_utils(env)
+ 
+  function new(string inst = "e", uvm_component parent = null);
+    super.new(inst,parent);
+  endfunction
+ 
+scoreboard s;
+agent a;
+ref_model rm;
+ 
+  virtual function void build_phase(uvm_phase phase);
+  super.build_phase(phase);
+    `uvm_info("ENV","env_built",UVM_LOW)
+    a = agent::type_id::create("a",this);
+    s = scoreboard::type_id::create("s",this);
+    rm =ref_model::type_id::create("rm",this);
+  endfunction
+ 
+
+  virtual function void connect_phase(uvm_phase phase);
+  super.connect_phase(phase);
+    `uvm_info("ENV",$sformatf("type_is:%s",a.m.get_type_name()),UVM_LOW)
+    a.m.send.connect(s.no_ref);
+    rm.send_2.connect(s.with_ref);
+  endfunction
+
+endclass
