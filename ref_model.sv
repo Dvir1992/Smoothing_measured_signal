40a69da926ad7f2cf04396a549749770aedce142
Dvir Hazut <53955899+Dvir1992@users.noreply.github.com>
2023-05-13 15:45:15 +0300

    Update ref_model.sv

--- ref_model.sv
+++ ref_model.sv
@@ -2,11 +2,11 @@ class ref_model extends uvm_component;
 	
   `uvm_component_utils(ref_model)
   uvm_analysis_port #(transaction) send_2;
-   virtual s_if vsif;  
-   integer j;
-   integer pixel_value;
-   integer pixel_counter;
-   transaction t;
+  virtual s_if vsif;  
+  integer j;
+  integer pixel_value;
+  integer pixel_counter;
+  transaction t;
   function new(input string inst = "ref_model", uvm_component parent = null);
     super.new(inst,parent); 
     `uvm_info(get_type_name(), $sformatf("ref_model"), UVM_LOW);
@@ -18,7 +18,7 @@ class ref_model extends uvm_component;
   super.build_phase(phase);    
     if(!uvm_config_db#(virtual s_if)::get(this,"","vsif",vsif))
       `uvm_error("RM", "Unable to access Interface");
-     t = transaction::type_id::create("t");
+    t = transaction::type_id::create("t");
 endfunction
 
   
@@ -36,13 +36,13 @@ virtual task run_phase(uvm_phase phase);
         forever begin
           @(negedge vsif.clk);
           if(vsif.vald_din) 
-        		t.data_in.push_back(vsif.data_in);                                
+        	t.data_in.push_back(vsif.data_in);                                
         end
       end
        begin
          forever begin
 		@(negedge vsif.vald_din);
-       //  `uvm_info("data_in",$sformatf("data in is:%p",t.data_in),UVM_LOW)
+      	        // `uvm_info("data_in",$sformatf("data in is:%p",t.data_in),UVM_LOW)
          end
       end
           
@@ -66,27 +66,19 @@ virtual task run_phase(uvm_phase phase);
            end 
             pixel_counter++; 
          end
-       //
        end        
       end
         begin
          forever begin
            @(negedge vsif.valid_out);           
-         //  `uvm_info("data_out",$sformatf("data out is:%p",t.data_out),UVM_LOW)
-            send_2.write(t);
-            pixel_counter=0;
-            t.data_out.delete();
-            t.data_in.delete();
-            j=0;
+           //`uvm_info("data_out",$sformatf("data out is:%p",t.data_out),UVM_LOW)
+           send_2.write(t);
+           pixel_counter=0;
+           t.data_out.delete();
+           t.data_in.delete();
+           j=0;
          end
-      end
-            
-     
-    join
-
-  
-  
-  
-endtask
-  
+      end                 
+    join     
+ endtask  
 endclass
